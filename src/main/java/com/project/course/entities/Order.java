33477042a2b0899e6529f9d28fce60b50eec4c3d
jonassbarai <jonas_sbarai@hotmail.com>
2022-05-29 11:09:08 -0300

    Criação do projeto  com as classes  User e Order e seus mapeamentos

---
+++ src/main/java/com/project/course/entities/Order.java
@@ -0,0 +1,105 @@
+package com.project.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.course.entities.enums.OrderStatus;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name ="tb_order")
+public class Order implements Serializable  {
+	private static final long serialVersionUID = 1L;
+	
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern ="yyyy-MM-dd'T' hh:mm:ss'Z'", timezone = "GMT")
+	 private Instant moment;
+	 @JsonIgnore
+	 @ManyToOne
+	 @JoinColumn(name ="Client_id")
+	 private User client;
+	 
+	 private  Integer orderStatus;
+	 
+	public Order() {
+
+	}
+
+	public Order(Long id, Instant moment,  OrderStatus orderStatus, User client) {
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}	
+
+	public OrderStatus getOrderStatus() {
+		
+		return OrderStatus.valueof(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+		 
+	
+}
---
+++ src/main/java/com/project/course/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.project.course.entities.enums;
+
+public enum OrderStatus {
+	WAITING_PAYMET(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+
+	private int code;
+	
+	OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueof(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+
+}
